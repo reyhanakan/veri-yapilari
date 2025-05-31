462fc395acf6bfde49be02f3b6e8ee350d7ff169
reyhan <167435118+reyhanakan@users.noreply.github.com>
2025-05-31 23:49:10 +0300

    Update Gen.hpp

--- Gen.hpp
+++ Gen.hpp
@@ -1,11 +1,4 @@
-/**
-* @file Gen.hpp
-* @description Gen sınıfının tanımlanması
-* @course 1.öğretim B grubu
-* @assignment 1.ödev
-* @date 6.11.24
-* @author Didem Reyhan AKAN / didem.akan@ogr.sakarya.edu.tr
-*/
+
 #ifndef GEN_HPP
 #define GEN_HPP
 #include <iostream>
