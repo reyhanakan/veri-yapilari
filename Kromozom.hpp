53045f8392298a3139a3b446fce39f2d61b9cccd
reyhan <167435118+reyhanakan@users.noreply.github.com>
2025-05-31 23:49:30 +0300

    Update Kromozom.hpp

--- Kromozom.hpp
+++ Kromozom.hpp
@@ -1,11 +1,4 @@
-/**
-* @file Kromozom.hpp
-* @description tanımların ve fonksiyonların isimleri
-* @course 1.öğretim B grubu
-* @assignment 1.ödev
-* @date 6.11.24
-* @author Didem Reyhan AKAN / didem.akan@ogr.sakarya.edu.tr
-*/
+
 #ifndef KROMOZOM_HPP
 #define KROMOZOM_HPP
 
