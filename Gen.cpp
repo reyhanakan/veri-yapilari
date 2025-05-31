5a3dcecc35433f1855e2c29300a9b71918e3dbac
reyhan <167435118+reyhanakan@users.noreply.github.com>
2025-05-31 23:49:02 +0300

    Update Gen.cpp

--- Gen.cpp
+++ Gen.cpp
@@ -1,11 +1,4 @@
-/**
-* @file Gen.cpp
-* @description Genin kurucu fonksiyonu 
-* @course 1.öğretim B grubu
-* @assignment 1.ödev
-* @date 6.11.24
-* @author Didem Reyhan AKAN / didem.akan@ogr.sakarya.edu.tr
-*/
+
 #include "Gen.hpp"
 
 Gen::Gen(string gen) {
